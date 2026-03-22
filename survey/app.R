14f4ed1a91f637846480ec434f53b04e8148f0c9
pingfan-hu <pingfan0727@gmail.com>
2026-03-22 11:18:35 -0400

    survey update

--- survey/app.R
+++ survey/app.R
@@ -1,49 +1,41 @@
-# Package setup ---------------------------------------------------------------
-
-# Install required packages:
-# install.packages("pak")
-# pak::pak("surveydown-dev/surveydown") # Development version from GitHub
-
-# Load packages
 library(surveydown)
+library(dplyr)
 
-# Database setup --------------------------------------------------------------
-#
-# Details at: https://surveydown.org/docs/storing-data
-#
-# surveydown stores data on any PostgreSQL database. We recommend
-# https://supabase.com/ for a free and easy to use service.
-#
-# Once you have your database ready, run the following function to store your
-# database configuration parameters in a local .env file:
-#
 # sd_db_config()
-#
-# Once your parameters are stored, you are ready to connect to your database.
-# For this demo, we set ignore = TRUE in the following code, which will ignore
-# the connection settings and won't attempt to connect to the database. This is
-# helpful if you don't want to record testing data in the database table while
-# doing local testing. Once you're ready to collect survey responses, set
-# ignore = FALSE or just delete this argument.
-
-db <- sd_db_connect()
-
-# UI setup --------------------------------------------------------------------
+db <- sd_db_connect(ignore = TRUE)
 
 ui <- sd_ui()
 
-# Server setup ----------------------------------------------------------------
-
 server <- function(input, output, session) {
-  # Define any conditional skip logic here (skip to page if a condition is true)
-  sd_skip_if()
+  # Generate a completion code for each respondent
+  completion_code <- sd_completion_code(8)
+  sd_store_value(completion_code)
 
-  # Define any conditional display logic here (show a question if a condition is true)
-  sd_show_if()
+  # Conditional skip logic - screen out people with no superhero interest
+  sd_skip_if(
+    input$has_fav_hero == "no" ~ "screenout"
+  )
 
-  # Run surveydown server and define database
-  sd_server(db = db)
+  # Conditional question display - only show hero name if they have a favorite
+  sd_show_if(
+    input$has_fav_hero == "yes" ~ "fav_hero_name"
+  )
+
+  # Create a reactive summary of their hero preferences
+  hero_summary <- sd_reactive("hero_summary", {
+    paste(
+      "Based on your responses, you prefer",
+      input$hero_universe,
+      "heroes with qualities like",
+      paste(input$hero_qualities, collapse = ", ")
+    )
+  })
+
+  # Main server function
+  sd_server(
+    db = db
+  )
 }
 
-# Launch the app
+# Launch the survey
 shiny::shinyApp(ui = ui, server = server)
